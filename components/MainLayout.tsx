4e2660355844839bff3197fd07e4cfd9ab60197c
Tai Zhang <tai.zhang@outlook.com>
2022-02-01 17:16:42 +0800

    Wrap NextUI link with Nextjs link

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Link from "next/link";
-import { Text, Container, Row, Card, Grid } from "@nextui-org/react";
+import NextLink from "next/link";
+import { Text, Container, Row, Card, Grid, Link } from "@nextui-org/react";
 import styles from "../styles/Home.module.css";
 
 const Layout: React.FC = ({ children }) => {
@@ -8,15 +8,13 @@ const Layout: React.FC = ({ children }) => {
     <Container>
       <Row>
         <h2 className={styles.footer}>
-          <Link href="/" passHref>
-            <Text
-              h1
-              size={60}
-              weight="bold"
-            >
-              ZHANGT.AI
-            </Text>
-          </Link>
+          <NextLink href="/" passHref>
+            <Link>
+              <Text h1 size={60} weight="bold">
+                ZHANGT.AI
+              </Text>
+            </Link>
+          </NextLink>
         </h2>
       </Row>
       {/*
@@ -46,29 +44,39 @@ const Layout: React.FC = ({ children }) => {
         <Grid.Container gap={2} justify="center">
           <Grid xs={12} lg={4}>
             <Card css={{ px: "$4" }}>
-              <Text
-                h1
-                size={60}
-                css={{
-                  textGradient: "45deg, $blue500 -20%, $pink500 50%",
-                }}
-                weight="bold"
-              >
-                <Link href="/blog">Blog</Link>
-              </Text>
-              <Text
-                h1
-                size={60}
-                css={{
-                  textGradient: "45deg, $purple500 -20%, $pink500 100%",
-                }}
-                weight="bold"
-              >
-                <Link href="/about">About</Link>
-              </Text>
+              <NextLink href="/blog">
+                <Link>
+                  <Text
+                    h1
+                    size={60}
+                    css={{
+                      textGradient: "45deg, $blue500 -20%, $pink500 50%",
+                    }}
+                    weight="bold"
+                  >
+                    Blog
+                  </Text>
+                </Link>
+              </NextLink>
+              <NextLink href="/about">
+                <Link>
+                  <Text
+                    h1
+                    size={60}
+                    css={{
+                      textGradient: "45deg, $purple500 -20%, $pink500 100%",
+                    }}
+                    weight="bold"
+                  >
+                    About
+                  </Text>
+                </Link>
+              </NextLink>
             </Card>
           </Grid>
-          <Grid xs={12} lg={8}>{children}</Grid>
+          <Grid xs={12} lg={8}>
+            {children}
+          </Grid>
         </Grid.Container>
       </Row>
       <Row>
