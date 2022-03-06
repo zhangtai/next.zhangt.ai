ec717e1d8a49da46110c1ab7ed7932c2f1c53e44
Tai Zhang <tai.zhang@outlook.com>
2022-03-06 10:57:29 +0800

    Update index layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import NextLink from "next/link";
-import { Container, Row, Col, Text, Link } from "@nextui-org/react";
+import { Container, Row, Col, Text, Link, Grid } from "@nextui-org/react";
 import Mermaid from "../components/Mermaid";
 
 import { Layout } from "../components/MainLayout";
@@ -16,45 +16,47 @@ const Home: NextPage = () => {
       </Head>
       <Layout>
         <Container>
-          <Row>
-            <Col
+          <Grid.Container gap={2} justify="center">
+            <Grid
+              xs={12}
+              lg={6}
               css={{
                 transform: "rotate(-15deg)",
               }}
             >
               <img alt="greeting" src="/images/greeting.svg" />
-              <img alt="rainbow" src="/images/rainbow.svg" />
-            </Col>
-            <Col
+            </Grid>
+            <Grid
+              xs={12}
+              lg={6}
               css={{
                 transform: "rotate(15deg)",
               }}
             >
               <NextLink href="/blog">
                 <Link>
-                    <img alt="blog" src="/images/blog.svg" />
+                  <img alt="blog" src="/images/blog.svg" />
                 </Link>
               </NextLink>
-            </Col>
-          </Row>
+            </Grid>
+          </Grid.Container>
           <Row justify="center" align="center">
-              <Mermaid
-                chart={`
-    graph RL
-    Blog[My Blog] --> Next[Next.js]
-    Blog --> NextUI[NextUI]
-    Blog --> Mermaid[Mermaidjs]
-    Next --> React[React]
-    React --> ts[TypeScript]
-    NextUI --> Geist[Geist]
-    Blog --> MDX[MDX]
-    `}
-              />
+            <Mermaid
+              chart={`
+                graph RL
+                Blog[My Blog] --> Next[Next.js]
+                Blog --> NextUI[NextUI]
+                Blog --> Mermaid[Mermaidjs]
+                Next --> React[React]
+                React --> ts[TypeScript]
+                NextUI --> Geist[Geist]
+                Blog --> MDX[MDX]
+              `}
+            />
           </Row>
         </Container>
         <Text>
-          Here is <FancyLink href="/about" text="why and how" /> I built
-          it.
+          Here is <FancyLink href="/about" text="why and how" /> I built it.
         </Text>
       </Layout>
     </>
